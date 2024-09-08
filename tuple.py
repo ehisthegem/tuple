48e002ea7ebd106d1a8e169d7bb8c69e845a481d
Israel Ozioma Idahosa <israelidahosa24@gmail.com>
2024-09-08 22:55:11 +0100

    Add files via upload

---
+++ tuple.py
@@ -0,0 +1,9 @@
+#Tuple is a collecton which is order and unchangeable.
+names = ('grandeur', 'senwin', 'ehis')
+print(names)
+print(len(names))
+print(type(names))
+
+names_2 = ('israel')
+print(names_2) 
+print(type(names_2))
